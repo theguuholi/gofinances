9841b97aed70f91776f74cf0447980a7a2255efa
Gustavo Oliveira <g.92oliveira@gmail.com>
2025-05-24 16:49:00 -0300

    themas com styled components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components/native';
 import Dashboard from './src/Screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 export default function App() {
-  return <Dashboard />
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
