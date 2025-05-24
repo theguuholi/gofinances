8e327eab68ed9a03055fe8ab95ae82708300ad93
Gustavo Oliveira <g.92oliveira@gmail.com>
2025-05-24 16:30:59 -0300

    install styled component

---
+++ src/Screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from "./style";
+
+const Dashboard = () => {
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    )
+}
+
+export default Dashboard;
