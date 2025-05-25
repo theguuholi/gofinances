dd44962d70f6f598f32f666be4ab8b800ccac008
Gustavo Oliveira <g.92oliveira@gmail.com>
2025-05-25 08:22:29 -0300

    add fonts

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from 'styled-components/native';
+import Dashboard from './src/Screens/Dashboard';
+import theme from './src/global/styles/theme';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
--- src/Screens/Dashboard/style.ts
+++ src/Screens/Dashboard/style.ts
@@ -11,5 +11,6 @@ export const Container = styled.View`
 export const Title = styled.Text`
     font-size: 24px;
     font-weight: bold;
+    font-family: ${({theme}) => theme.fonts.bold};
     color: ${({theme}) => theme.colors.title};
 `; 
