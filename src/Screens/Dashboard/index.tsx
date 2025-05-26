6ace0efd176c6f0ab07c8da9a24e944e73e1d93e
Gustavo Oliveira <g.92oliveira@gmail.com>
2025-05-26 14:30:42 -0300

    creating header

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,9 +1,20 @@
-import { Container, Title } from "./style";
+import { Container, Header, Photo, User, UserContainer, UserGreeting, UserInfo, UserName } from "./style";
 
 const Dashboard = () => {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserContainer>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/12762300?v=4' }} />
+
+                        <User>
+                            <UserGreeting>Ola, </UserGreeting>
+                            <UserName>Gustavo</UserName>
+                        </User>
+                    </UserInfo>
+                </UserContainer>
+            </Header>
         </Container>
     )
 }
